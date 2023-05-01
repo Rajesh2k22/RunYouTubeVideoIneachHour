a8894187e91d6d000b1ebe0e87b1f6efa7ce1e30
Rajesh2k22 <102841802+Rajesh2k22@users.noreply.github.com>
2023-05-01 23:06:21 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,8 @@
+
+
+ setInterval(function (){
+      
+       const ele= document.getElementById("Container");
+
+        ele.innerHTML=`<iframe width="100%" height="100%" src="https://www.youtube.com/embed/lXs5wE0qa3w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+ },3600000);
